a15e1dd25c3a270b6d8b333e4d835e69560a7221
Christian Heyn <chris.heyn.dd@gmail.com>
2017-08-09 12:33:53 +0200

    refactored func names

--- src/index.hs
+++ src/index.hs
@@ -25,28 +25,34 @@ convertStringToFloat s =
         asFloat = read betterString :: Float
     in asFloat
 
-tellUnit :: String -> String
-tellUnit value 
+getUnit :: String -> String
+getUnit value 
     | value `containsChars` "m" = "Minutes"
     | value `containsChars` "h" = "Hours"
     | value `containsChars` "d" = "Days"
     | otherwise = "Hours"
 
+getMinutesFrom :: Float -> String -> Float
+getMinutesFrom t unit
+    | unit == "Hours" = t * 60
+    | unit == "Days" = t * 8 * 60
+    | otherwise = t
+
 -- Main
 main = do  
-    putStrLn "Likely case?"
-    likely <- getLine
-    putStrLn "Worst case?"
-    worst <- getLine
-    putStrLn "Best case?"
-    best <- getLine
+    print "Likely case?"
+    likelyInput <- getLine
+    print "Worst case?"
+    worstInput <- getLine
+    print "Best case?"
+    bestInput <- getLine
 
-    let l = tellUnit likely
-        w = tellUnit worst
-        b = tellUnit best
+    let likelyUnit = getUnit likelyInput
+        worstUnit = getUnit worstInput
+        bestUnit = getUnit bestInput
+        likelyFloat = convertStringToFloat likelyInput
+        worstFloat = convertStringToFloat worstInput
+        bestFloat = convertStringToFloat bestInput
 
-
-    putStrLn ("Likely case: " ++ l)
-    putStrLn ("Worst case: " ++ w)
-    putStrLn ("Best case: " ++ b)
-    print (convertStringToFloat "15,00")
+    putStrLn ("Likely case: " ++ likelyUnit)
+    print (getMinutesFrom 2 "Days")
